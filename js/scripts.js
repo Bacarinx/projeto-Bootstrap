353505dc60628129cef704a4cc209f2995ab1225
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-02-21 08:30:51 -0300

    parallax agency

--- js/scripts.js
+++ js/scripts.js
@@ -81,6 +81,8 @@ $(document).ready(function () {
     // Parralax
     setTimeout(function () {
         $('#data-area').parallax({imageSrc:'img/cidadeparallax.png'})
+        $('#apply-area').parallax({imageSrc:'img/pattern.png'})
     }, 250)
+
 
 })
